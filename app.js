117f624765d4b3ce2b7356a83e403a593483d960
LinCie <aldiofernanda@protonmail.com>
2024-01-07 16:24:11 +0700

    move app into its own module

---
+++ app.js
@@ -0,0 +1,29 @@
+// Utility
+const config = require("./utils/config");
+const middleware = require("./utils/middleware");
+
+// Express stuffs
+const cors = require("cors");
+const express = require("express");
+const morgan = require("morgan");
+
+// Routers
+const blogsRouters = require("./controllers/blogs");
+
+// DB Stuffs
+const mongoose = require("mongoose");
+
+const app = express();
+
+mongoose.connect(config.MONGODB_URI);
+
+app.use(morgan("tiny"));
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/blogs", blogsRouters);
+
+app.use(middleware.errorHandler);
+app.use(middleware.unknownEndpoint);
+
+module.exports = app;
--- index.js
+++ index.js
@@ -1,30 +1,7 @@
-// Utility
-const logger = require("./utils/logger");
 const config = require("./utils/config");
-const middleware = require("./utils/middleware");
+const logger = require("./utils/logger");
 
-// Express stuffs
-const express = require("express");
-const app = express();
-const morgan = require("morgan");
-const cors = require("cors");
-
-// Routers
-const blogsRouters = require("./controllers/blogs");
-
-// DB Stuffs
-const mongoose = require("mongoose");
-
-mongoose.connect(config.MONGODB_URI);
-
-app.use(cors());
-app.use(express.json());
-app.use(morgan("tiny"));
-
-app.use("/api/blogs", blogsRouters);
-
-app.use(middleware.errorHandler);
-app.use(middleware.unknownEndpoint);
+const app = require("./app");
 
 const PORT = config.PORT;
 app.listen(PORT, () => {
