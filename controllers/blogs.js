611bb9f28f748f576dc6736a907e288d698b414e
LinCie <aldiofernanda@protonmail.com>
2024-01-07 16:17:22 +0700

    added middleware and use next on routes

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,23 +1,21 @@
 const blogsRouter = require("express").Router();
 const BlogServices = require("../models/services/BlogServices");
 
-const logger = require("../utils/logger");
-
-blogsRouter.get("/", async (request, response) => {
+blogsRouter.get("/", async (request, response, next) => {
   try {
     const blogs = await BlogServices.getBlogs();
     response.status(200).json(blogs);
   } catch (err) {
-    logger.error(err);
+    next(err);
   }
 });
 
-blogsRouter.post("/", async (request, response) => {
+blogsRouter.post("/", async (request, response, next) => {
   try {
     const blog = await BlogServices.postBlog(request.body);
     response.status(201).json(blog);
   } catch (err) {
-    logger.error(err);
+    next(err);
   }
 });
 
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Utility
 const logger = require("./utils/logger");
 const config = require("./utils/config");
+const middleware = require("./utils/middleware");
 
 // Express stuffs
 const express = require("express");
@@ -21,6 +22,9 @@ app.use(express.json());
 app.use(morgan("tiny"));
 
 app.use("/api/blogs", blogsRouters);
+
+app.use(middleware.errorHandler);
+app.use(middleware.unknownEndpoint);
 
 const PORT = config.PORT;
 app.listen(PORT, () => {
