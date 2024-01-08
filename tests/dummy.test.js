17850692d85429e5f57aa67c39c657270400f071
LinCie <aldiofernanda@protonmail.com>
2024-01-08 14:24:01 +0700

    exercise 4.4 submission

--- tests/dummy.test.js
+++
@@ -1,8 +0,0 @@
-const listHelper = require("../utils/listHelper");
-
-test("dummy returns 1", () => {
-  const blogs = [];
-
-  const result = listHelper.dummy(blogs);
-  expect(result).toBe(1);
-});
--- utils/listHelper.js
+++ utils/listHelper.js
@@ -1,7 +1,14 @@
-const dummy = (blogs) => {
-  return 1;
-};
+const dummy = (blogs) => 1;
+
+const totalLikes = (blogs) =>
+  blogs
+    // Get all the likes from the blogs
+    .map((blog) => blog.likes)
+
+    // Sum the likes
+    .reduce((a, b) => a + b, 0);
 
 module.exports = {
   dummy,
+  totalLikes,
 };
