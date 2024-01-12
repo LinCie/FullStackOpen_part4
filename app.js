5a9d50446e2da8133aa8cbdd8b4dd02c8a8bb1fd
LinCie <aldiofernanda@protonmail.com>
2024-01-12 14:11:10 +0700

    change morgan to custom logger

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ require("express-async-errors");
 // Express stuffs
 const cors = require("cors");
 const express = require("express");
-const morgan = require("morgan");
 
 // Routers
 const blogsRouters = require("./controllers/blogs");
@@ -18,7 +17,7 @@ const app = express();
 
 mongoose.connect(config.MONGODB_URI);
 
-app.use(morgan("tiny"));
+app.use(middleware.requestLogger);
 app.use(express.json());
 app.use(cors());
 
